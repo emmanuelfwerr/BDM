9875cf8e92815f383dc425307f4c82a625b126e0
meri-arbiol <105277450+meri-arbiol@users.noreply.github.com>
2022-06-17 13:49:36 +0200

    update model + streaming meri

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from alldata_format import RDDIntegration
-#from KPI_exploit import generateKPIs
+#from alldata_format import RDDIntegration
+from KPI_exploit import generateKPIs
 #import streaming_spark
 
 if __name__ == '__main__':
-    RDDIntegration()
-    #generateKPIs()
+    #RDDIntegration()
+    generateKPIs()
     #streaming_spark()
