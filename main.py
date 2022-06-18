3a91f3a662d15b56f957606c3f6d0b50fee21553
meri-arbiol <105277450+meri-arbiol@users.noreply.github.com>
2022-06-18 02:33:22 +0200

    update kpi_exploit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #from alldata_format import RDDIntegration
 from KPI_exploit import generateKPIs
-from streaming_spark import streaming_prediction
+#from streaming_spark import streaming_prediction
 
 if __name__ == '__main__':
     #RDDIntegration()
